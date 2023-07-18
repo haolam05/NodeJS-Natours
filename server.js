a638aae5a6d09ee9b83b71a1845792608424535d
Hao Lam <tuonghao2001@gmail.com>
2023-07-18 02:50:34 -0700

    connected to hosted databse (Atlas)

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const port = 3000;
-app.listen(port, () => console.log(`App running on port ${port}`));
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(con => console.log('DB is successfully connected!'));
+
+app.listen(process.env.PORT, () =>
+  console.log(`App running on port ${process.env.PORT}`),
+);
