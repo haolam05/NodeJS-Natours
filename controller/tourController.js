20a5808d02858bfc22efd8e1c1f59dcfae3be7ed
Hao Lam <tuonghao2001@gmail.com>
2023-07-19 00:30:05 -0700

    getAllTours with query

--- controller/tourController.js
+++ controller/tourController.js
@@ -2,7 +2,15 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILDING QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+    const query = Tour.find(queryObj);
+
+    // EXECUTE QUERY
+    const tours = await query;
+
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
