f00e56ac8a9bd6f9d8bac86a74aaa0321fba8a8b
Hao Lam <tuonghao2001@gmail.com>
2023-07-19 01:08:52 -0700

    sorting for getAllTours

--- controller/tourController.js
+++ controller/tourController.js
@@ -3,18 +3,25 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // BUILDING QUERY
-    console.log(req.query);
-    // 1) Filtering
+    // 1a) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
 
-    // 2) Advanced Filtering
+    // 1b) Advanced Filtering
     // { duration: { gt: '5' } } -> { duration: { $gt: 5 } }
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`);
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // EXECUTE QUERY
     const tours = await query;
