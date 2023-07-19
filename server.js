f9d8311fef83e14c467d0c4efac552d9fd13e53c
Hao Lam <tuonghao2001@gmail.com>
2023-07-18 21:33:06 -0700

    create schema to validate date using mongoose

--- server.js
+++ server.js
@@ -15,6 +15,23 @@ mongoose
     useFindAndModify: false,
   })
   .then(con => console.log('DB is successfully connected!'));
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+console.log(Tour.collection);
 
 app.listen(process.env.PORT, () =>
   console.log(`App running on port ${process.env.PORT}`),
