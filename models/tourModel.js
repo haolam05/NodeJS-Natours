6a8616a94783b42466cc43d2f2e92e30f8265948
Hao Lam <tuonghao2001@gmail.com>
2023-07-20 09:06:11 -0700

    mongoose aggregation middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -82,10 +82,15 @@ tourSchema.pre(/^find/, function (next) {
   this.start = Date.now();
   next();
 });
-
 tourSchema.post(/^find/, function (docs, next) {
-  console.log(`Query took ${Date.now() - this.start}`);
-  console.log(docs);
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  // console.log(docs);
+  next();
+});
+
+// Aggregation middleware
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
   next();
 });
 
