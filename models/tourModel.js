471f2e4501117b5e9de02a819f02c6f97191415d
Hao Lam <tuonghao2001@gmail.com>
2023-07-20 09:33:12 -0700

    mongoose custom validators & added validator package

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const slugify = require('slugify');
+const validator = require('validator');
 const mongoose = require('mongoose');
 
 const tourSchema = new mongoose.Schema(
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name can not have more than 40 characters.'],
       minlength: [10, 'A tour name can not have less than 10 characters.'],
+      validate: [validator.isAlpha, 'Tour name must only contain letters!'],
     },
     slug: String,
     duration: {
@@ -42,7 +44,15 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) must be smaller than current price.',
+      },
+    },
     summary: {
       type: String,
       trim: true,
