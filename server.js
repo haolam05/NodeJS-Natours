708848a718997e024d028b917905bcf54e5bd206
Hao Lam <tuonghao2001@gmail.com>
2023-07-23 02:54:54 -0700

    handle errors outside of express (unhandle rejection)

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ mongoose
   })
   .then(() => console.log('DB is successfully connected!'));
 
-app.listen(process.env.PORT, () =>
+const server = app.listen(process.env.PORT, () =>
   console.log(`App running on port ${process.env.PORT}`),
 );
+
+process.on('unhandledRejection', err => {
+  console.log(`\nError: ${err.name}\nMessage: ${err.message}\n`);
+  console.log('UNHANDLED REJECTION! ❌ Shutting down ...');
+  server.close(() => process.exit(1));
+});
