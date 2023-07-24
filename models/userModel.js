a2576aff9d8d75cd09ec4565a8f80498d5859e33
Hao Lam <tuonghao2001@gmail.com>
2023-07-23 23:27:04 -0700

    encrypt passwords using bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,32 +1,47 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'Please tell us your name'],
-    },
-    email: {
-      type: String,
-      required: [true, 'Please provide your email'],
-      unique: true,
-      lowercase: true, // not a validator, just conveted to lowercase
-      validate: [validator.isEmail, 'Please provide a valid email'],
-    },
-    photo: String,
-    password: {
-      type: String,
-      required: [true, 'Please provide a password!'],
-      minlength: 8,
-    },
-    passwordConfirm: {
-      type: String,
-      required: [true, 'Please confirm your password!'],
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true, // not a validator, just conveted to lowercase
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (passwordConfirm) {
+        // only works on save() and create()
+        return passwordConfirm === this.password;
+      },
+      message: 'Passwords are not the same',
     },
   },
-  {},
-);
+});
+
+userSchema.pre('save', async function (next) {
+  // encrypt when password is actually modified (on save() & create())
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined;
+  }
+
+  next();
+});
 
 const User = mongoose.model('User', userSchema);
 
