7d08845def39d691e9f776c007d0889b0606b57f
Hao Lam <tuonghao2001@gmail.com>
2023-07-25 21:25:53 -0700

    send jwt via cookie

--- controller/authController.js
+++ controller/authController.js
@@ -13,6 +13,17 @@ const signToken = id =>
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true, // prevent browser to access or modify the cookie in any way
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true; // https
+  res.cookie('jwt', token, cookieOptions);
+  user.password = undefined; // remove password from output
+
   res.status(statusCode).json({
     status: 'success',
     token,
