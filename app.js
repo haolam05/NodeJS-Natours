58ad8b3d4fbd67e9ee4aef25dd59ec57e267123d
Hao Lam <tuonghao2001@gmail.com>
2023-07-25 21:32:50 -0700

    implement rateLimiter on all /api routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const globalErrorHandler = require('./controller/errorController');
@@ -7,8 +8,16 @@ const AppError = require('./utils/appError');
 
 const app = express();
 
-// MIDDLEWARES
+// Helper functions
+const limiter = rateLimit({
+  max: 100, // 100 requests per hour
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+// GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+app.use('/api', limiter);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
