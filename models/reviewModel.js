9ea879b911c6b0bbeafd0fab72cf7e3039dbc303
Hao Lam <tuonghao2001@gmail.com>
2023-07-27 23:43:49 -0700

    implement reviewModel

---
+++ models/reviewModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty!'],
+    },
+    rating: {
+      type: Number,
+      max: [5, 'Rating can not be more than 5.0'],
+      min: [1, 'Rating can not be less than 1.0'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour.'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user.'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+exports.module = Review;
