f8cb2c587a02e0f79ee906c4fcc1d4c2f109a555
Hao Lam <tuonghao2001@gmail.com>
2023-07-28 21:55:22 -0700

    created factory handler function for createOne

--- controller/handlerFactory.js
+++ controller/handlerFactory.js
@@ -17,15 +17,29 @@ exports.deleteOne = Model =>
 
 exports.updateOne = Model =>
   catchAsync(async (req, res, next) => {
-    const dcoument = await Model.findByIdAndUpdate(req.params.id, req.body, {
+    const document = await Model.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true,
     });
-    if (!dcoument)
-      return next(new AppError('No dcoument found with that ID', 404));
+    if (!document)
+      return next(new AppError('No document found with that ID', 404));
 
     res.status(200).json({
       status: 'success',
-      data: { data: dcoument },
+      data: {
+        data: document,
+      },
+    });
+  });
+
+exports.createOne = Model =>
+  catchAsync(async (req, res) => {
+    const newDocument = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: newDocument,
+      },
     });
   });
