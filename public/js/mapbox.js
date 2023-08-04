d1e05babfa91895a5e50502c2bbd7bf66abd7de1
Hao Lam <tuonghao2001@gmail.com>
2023-08-04 00:53:29 -0700

    display map on tour page

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,3 +1,11 @@
 /* eslint-disable */
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiaGFvbGFtMDUiLCJhIjoiY2xrdzl5Yzh5MGk3bTNjbXo0Z3dvdml5NCJ9.SU6gr-0RZENJCLOPp6W40g';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v10',
+});
