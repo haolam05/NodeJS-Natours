8ec51502ce61ee72ee0566635295b92cfe082b89
Hao Lam <tuonghao2001@gmail.com>
2023-08-04 01:13:07 -0700

    use mapbox to display tour locations on map

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log(locations);
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoiaGFvbGFtMDUiLCJhIjoiY2xrdzl5Yzh5MGk3bTNjbXo0Z3dvdml5NCJ9.SU6gr-0RZENJCLOPp6W40g';
@@ -8,4 +7,39 @@ mapboxgl.accessToken =
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/light-v10',
+  scrollZoom: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(location => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(location.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({ offset: 30 })
+    .setLngLat(location.coordinates)
+    .setHTML(`<p>Day ${location.day}: ${location.description}</p>`)
+    .addTo(map);
+
+  // Extends map bounds to include current location
+  bounds.extend(location.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 200,
+    right: 100,
+  },
 });
