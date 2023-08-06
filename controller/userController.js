9f2bc840dd4dd3a9bff757d7a710c76ab1a59d98
Hao Lam <tuonghao2001@gmail.com>
2023-08-05 19:55:56 -0700

    saved user-upload-photo and add default profile picture

--- controller/userController.js
+++ controller/userController.js
@@ -40,8 +40,6 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file);
-  console.log(req.body);
   // 1) Throw error if user posted password data
   if (req.body.password || req.body.passwordConfirm)
     return next(
@@ -53,6 +51,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   // 2) Filtered out unwanted filed names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
