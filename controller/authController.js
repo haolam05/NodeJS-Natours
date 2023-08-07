588985a0f11bd381834575a7df118e0cedec1239
Hao Lam <tuonghao2001@gmail.com>
2023-08-07 10:27:46 -0700

    sending password reset emails

--- controller/authController.js
+++ controller/authController.js
@@ -152,16 +152,17 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // 3) Send it to user's email
-  const resetURL = `${req.protocol}://${req.get('host')}/api/${
-    process.env.VERSION
-  }/users/resetPassword/${resetToken}`;
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\n If you didn't forget your password, please ignore this email!`;
+
   try {
-    // await sendEmail({
-    //   email: user.email,
-    //   subject: 'Your password reset token (valid for 10 min)',
-    //   message,
-    // });
+    const resetURL = `${req.protocol}://${req.get('host')}/api/${
+      process.env.VERSION
+    }/users/resetPassword/${resetToken}`;
+    await new Email(user, resetURL).sendPasswordReset();
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email!',
+    });
   } catch (err) {
     user.passwordResetToken = undefined;
     user.passwordResetExpires = undefined;
@@ -173,11 +174,6 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
       ),
     );
   }
-
-  res.status(200).json({
-    status: 'success',
-    message: 'Token sent to email!',
-  });
 });
 
 exports.resetPassword = catchAsync(async (req, res, next) => {
