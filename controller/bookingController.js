f31fb035cefa54ac8494a7c73a476f6b221a2b62
Hao Lam <tuonghao2001@gmail.com>
2023-08-07 23:08:12 -0700

    integrate stripe into backend

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -1,3 +1,4 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
@@ -6,6 +7,35 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.tourId);
 
   // 2) Create a checkout session
+  const session = await stripe.checkout.sessions.create({
+    // required
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+
+    // optional
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourId,
+    line_items: [
+      {
+        quantity: 1,
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`], // must be live images hosted on internet
+          },
+        },
+      },
+    ],
+    mode: 'payment',
+  });
 
   // 3) Create session as response
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
 });
