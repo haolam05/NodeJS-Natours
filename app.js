886be538cd53a68e512b28750d2e74643b95aba7
Hao Lam <tuonghao2001@gmail.com>
2023-08-08 01:45:00 -0700

    install compression

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -70,6 +71,9 @@ app.use(
     ],
   }),
 );
+
+// Compression
+app.use(compression());
 
 // Test middleware
 app.use((req, res, next) => {
